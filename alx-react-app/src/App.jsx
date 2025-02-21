b3c1e44bc707f2c6fb4f6ae851515bd3437f3186
Oloke Emma <emmanueloloke4@gmail.com>
2025-02-21 20:30:24 +0100

    my react program

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <>
       <WelcomeMessage />
       <Header />
-      <MainContent/>
-      <Footer/>
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
