3e0b6403b348033e56b6a22fa8e4088bf12bd707
Oloke Emma <emmanueloloke4@gmail.com>
2025-02-23 12:25:22 +0100

    Initial commit

---
+++ scr/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '20px', backgroundColor: '#333', color: '#fff' }}>
+      <p>© 2025 Our Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
