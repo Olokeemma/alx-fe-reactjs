ca038904a4b0087d5683d5a0f81618ef9ddd32af
Oloke Emma <emmanueloloke4@gmail.com>
2025-03-16 09:56:19 +0100

    taildcss committing

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -9,3 +9,15 @@ function App() {
 }
 
 export default App;
+// src/App.js
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
